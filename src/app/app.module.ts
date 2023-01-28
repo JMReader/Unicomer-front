7f9deb46b050437871c3c00fc35695dd392feb6b
koritacrah <juanmcoro2003@gmail.com>
2023-01-28 11:40:06 -0300

    maqueta login
    finalizacion maqueta html y css login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './component/layout/sidebar/sidebar.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
