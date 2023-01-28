8cb7066b95ef49c68b6b9a2a7fcf3fe0b8e5ee03
koritacrah <juanmcoro2003@gmail.com>
2023-01-28 13:55:42 -0300

    Revert "Revert "cambios para actualizar""
    This reverts commit e371456ca4d39ded063a28442db3a279b475d58b.

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons';
+import Keyboard from "simple-keyboard";
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  value = "";
+  keyboard!: Keyboard;
+  faKeyboard = faKeyboard;
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngAfterViewInit() {
+    this.keyboard = new Keyboard({
+      onChange: input => this.onChange(input),
+      onKeyPress: (button:any) => this.onKeyPress(button)
+    });
+  }
+
+  onChange = (input: string) => {
+    this.value = input;
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = (button: string) => {
+    console.log("Button pressed", button);
+
+ 
+  };
+
+  onInputChange = (event: any) => {
+    this.keyboard.setInput(event.target.value);
+  };
 
 }
